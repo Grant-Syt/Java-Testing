eff43997849b0add001f12d64d35ff4a35c2ca41
Grant-Syt <grantsyt21@gmail.com>
2021-01-18 16:15:25 -0500

    CustomStack in one file

--- Test/src/test/CustomStackTest.java
+++ Test/src/test/CustomStackTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class CustomStackTest {
 	public static void main(String[] args) {
 		CustomStack cs = new CustomStack();
@@ -37,4 +40,46 @@ public class CustomStackTest {
 		System.out.println(cs.pop());
 		System.out.println(cs.pop() + "\n");
 	}
+}
+
+class CustomStack {
+	private ArrayList<Integer> incArr;
+    private Stack<Integer> st;
+
+    CustomStack() {
+    	incArr = new ArrayList<Integer>();
+    	st = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+            st.push(x);
+            incArr.add(0);
+    }
+
+    public int pop() {
+        if(st.size() == 0) {
+        	return -1;
+        }
+        int poppedInt = st.peek();
+        st.pop();
+        int incIndex = incArr.size()-1;
+        // increment values as they are popped and move the increment info down
+        if(incArr.get(incIndex) != 0){
+        	poppedInt += incArr.get(incIndex);
+        	if (incIndex != 0) {
+        		incArr.set(incIndex-1, incArr.get(incIndex) 
+                		+ incArr.get(incIndex-1));
+        	}
+        }
+        incArr.remove(incIndex);
+        return poppedInt;
+    }
+
+    // increment last k elements
+    public void increment(int k, int val) {
+    	int incIndex = incArr.size()-1;
+        incArr.set(Math.min(k-1, incIndex), val
+        		+ incArr.get(incIndex));
+    }
+    
 }
